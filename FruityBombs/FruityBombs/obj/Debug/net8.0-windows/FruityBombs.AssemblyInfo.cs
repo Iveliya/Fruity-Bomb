7853dd5bd375744969e46f3d3810fe71891d8c26
Ivan Ivanov <fenix4win@gmail.com>
2024-11-06 09:37:38 +0200

    Fix

--- FruityBombs/FruityBombs/obj/Debug/net8.0-windows/FruityBombs.AssemblyInfo.cs
+++ FruityBombs/FruityBombs/obj/Debug/net8.0-windows/FruityBombs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FruityBombs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+effaa37b355f8ab4ec2a017d8b5de0fb0cdeecbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a41b2e5a0029fdead70c80d2dd9f12444e9fd72")]
 [assembly: System.Reflection.AssemblyProductAttribute("FruityBombs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FruityBombs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
